04e3fe4490b0552d8eb3b3e84c25b58eb86ec295
DIDIVADAPAV <98083428+DIDIVADAPAV@users.noreply.github.com>
2022-01-20 15:01:50 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,26 @@
+var MenuItems = document.getElementById("MenuItems");
+    MenuItems.style.maxHeight = "0px";
+
+    function menutoggle(){
+        if(MenuItems.style.maxHeight == "1679px");
+
+        {
+            MenuItems.style.maxHeight = "100px";
+
+        
+
+    }
+        
+
+
+
+        
+          
+        
+    
+        
+
+
+    
+        
+    }
